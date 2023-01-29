b86c48be19337a18f3bc154ed92a66e3f1834e5e
ctienon <tienoncelestin@gmail.com>
2023-01-29 13:25:42 -0500

    Updated fev 29

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,12 +1,45 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+import ExpenseTotal from '../components/ExpenseTotal'
 
-const Budget = () => {
+const Budget = (props) => {
     const { budget } = useContext(AppContext);
+    const { currency } = useContext(AppContext);
+    const { dispatch } = useContext(AppContext);
+    const { remaining } = useContext(AppContext);
 
+    const UpdateBudget = (amount) => {
+        
+        if (amount > 20000) {
+            alert('The budget cannot exceed 20 000 £');
+            return;
+        } 
+        if( remaining <= 0 ) {
+            alert('The budget cannot be lower than the spending');
+            return;
+
+        } else {
+           let budget = {amount};
+        }
+    
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: amount
+        });
+    };
+   
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <label>Budget ({currency}):</label>
+            <input
+                required='required'
+                type='number'
+                id='cost'
+                step ='10'
+                value={budget}
+                style={{ marginLeft: '2rem' , size: 10}}
+                onChange={(event) => UpdateBudget(event.target.value)}>
+            </input>
         </div>
     );
 };
